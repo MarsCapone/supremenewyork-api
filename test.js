e8d5f72be1261eed59ba3586a8b5af86090b9499
Rock <arockplaysmc@gmail.com>
2017-12-28 12:45:07 -0800

    Added new function 'droplist'

--- test.js
+++ test.js
@@ -1,9 +1,17 @@
 const api = require('./index.js');
 
-api.find('sled', 'all', (item, error) => {
+api.droplist((droplist, error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(droplist);
+  }
+})
+
+/*api.find('sled', 'all', (item, error) => {
   if (error) {
     console.log(error);
   } else {
     console.log(item);
   }
-});
+});*/
